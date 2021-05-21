914e7f6be6db10efafbc52c38a318de5e2b521a2
Rob Lopez <rob.lopez@gmail.com>
2021-05-20 18:30:04 -0700

    Updates main instructions

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-/*
-get a random number less than 10,
-then, wait three seconds
-then, write the number to the hard drive
-Then, once all that is done, console log "I'm done!"
-
+/* 
+Console Output when you run this file should be: 
+waited one second
+waited one second
+waited one second
+I'm done
+and randomNumber/numFile.txt should exist with a random number in it.
 CONSTRAINTS
-- You must use "getNumLessThan.js" to generate your random number. Note: the "getNumLessThan.js" file is just an overly complicated way to make getting a random number asynchronous (there is no reason for it to be asynchronous, other than as an exercise).
-- No editing any file except main.js (comments are the exception)
+- You cannot use Math.random or the node fs module directly. 
 */
+
+
 
 
 
